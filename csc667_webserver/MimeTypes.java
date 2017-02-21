21e126200dcc9b3b2d59658f6f6cc1f29a3302de
Eldres <Josh@Joshs-PC>
2017-02-21 15:47:22 -0800

    added local variables fileReader,bufferedReader to load()

--- csc667_webserver/MimeTypes.java
+++ csc667_webserver/MimeTypes.java
@@ -23,12 +23,14 @@ public class MimeTypes extends ConfigurationReader
     {
     	super(fileNames);
         this.types = new Hashtable(); //HashMap<>();
-        this.bufferedReader = new BufferedReader(new FileReader(this.getFile()));
+        //this.bufferedReader = new BufferedReader(new FileReader(this.getFile()));
         this.load();
     }
 
     public void load() throws IOException
     {        
+        FileReader fileReader = new FileReader(this.getFile());
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
         types = new Hashtable<String, String>();
         
         String line;
@@ -42,7 +44,7 @@ public class MimeTypes extends ConfigurationReader
 
     public String lookup(String extension)
     {
-        
+        //return types.get(extension); not 100% sure on this statement
         return "";
     }
 }
